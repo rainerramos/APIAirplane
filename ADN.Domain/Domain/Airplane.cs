5819dab43860846c86907eb1d4cfdcbbc9ff3676
rainerramos <rainermr89@hotmail.com>
2022-09-13 10:24:19 -0300

    criando domain

--- ADN.Domain/Domain/Airplane.cs
+++ ADN.Domain/Domain/Airplane.cs
@@ -2,5 +2,12 @@
 {
     public class Airplane
     {
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public TimeOnly FlightHours { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Width { get; set; }
+        public int Height { get; set; }
     }
 }
