a80a874b21079a991974652cedb3a517c128b79b
rainerramos <rainermr89@hotmail.com>
2022-09-16 10:21:22 -0300

    adicionando data

---
+++ ADN.Data/Repository/AirplaneRepository.cs
@@ -0,0 +1,26 @@
+﻿using ADN.Domain.Domain;
+using ADN.Domain.DTO.Settings;
+using ADN.Domain.Interfaces.Repository;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ADN.Data.Repository
+{
+    public class AirplaneRepository : IAirplaneRepository
+    {
+        private readonly IMongoCollection<Airplane> _collection;
+
+        public AirplaneRepository(IOptions<MongoDBAirplaneSettings> mongoAirplaneSettings )
+        {
+            var mongoClient = new MongoClient(mongoAirplaneSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(mongoAirplaneSettings.Value.DatabaseName);
+            _collection = mongoDatabase.GetCollection<Airplane>(mongoAirplaneSettings.Value.CollectionName);
+        }
+
+        public async Task<List<Airplane>> GetAll()
+        {
+            var result = await _collection.FindAsync(c => true);
+            return result.ToList(); 
+        }
+    }
+}
---
+++ ADN.Domain/Interfaces/Repository/IAirplaneRepository.cs
@@ -0,0 +1,9 @@
+﻿using ADN.Domain.Domain;
+
+namespace ADN.Domain.Interfaces.Repository
+{
+    public interface IAirplaneRepository
+    {
+        Task<List<Airplane>> GetAll();
+    }
+}
